2ccd247ebfb32fae191974b0c8d5b87aee6313cc
Mehedi <mehedi.interface@gmail.com>
2026-06-04 09:02:58 +0600

    Example 1

---
+++ example-1.js
@@ -0,0 +1,12 @@
+const products = [
+    "Mosquito Coil", 
+    "Tea Leaf", 
+    "Condensed Milk", 
+    "Soap"
+]; 
+
+console.log(products[0]); 
+
+products.push("Detergent Powder"); 
+
+console.log(products)
