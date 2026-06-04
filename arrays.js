c4b1646907c97e296bcefcdad3960eada6200e26
Mehedi <mehedi.interface@gmail.com>
2026-06-04 08:44:10 +0600

    JavaScript Arrays

---
+++ arrays.js
@@ -0,0 +1,29 @@
+// A JavaScript Array is a data structure used to store multiple values in a single variable. 
+
+// array 
+const fruits = ["Apple", "Banana", "Mango"]; 
+
+// Accessing Array Elements: Array indexes start from 0 
+console.log(fruits[0]); 
+console.log(fruits[1]); 
+console.log(fruits[2]); 
+
+// Getting the length of an array 
+console.log(fruits.length); 
+
+// Add to the End (push)
+fruits.push("Watermelon"); 
+console.log(fruits); 
+
+// Add to the Beginning (unshift)
+fruits.unshift("Orange"); 
+console.log(fruits); 
+
+// Remove from the end (pop)
+fruits.pop(); 
+console.log(fruits); 
+
+// Remove from the Beginning (shift)
+fruits.shift(); 
+console.log(fruits); 
+
